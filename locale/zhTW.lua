605465b8757a69dbe656624410aac3c5064c1ec9
leg883 <leg883@126.com>
2013-01-09 08:29:18 -0800

    Create zhTW.lua

---
+++ locale/zhTW.lua
@@ -0,0 +1,25 @@
+﻿
+local L = tdCore:NewLocale('tdCore', 'zhTW')
+
+if not L then return end
+
+L['Abort']      = '退出'
+L['Discard']    = '放棄'
+L['Open']       = '開啟'
+L['Retry']      = '重試'
+
+L['Taiduo\'s Addons'] = '太多插件'
+
+L['You change the configuration of some addons, you want to save ?'] = '你修改了部份插件的配置，是否保存？'
+L['Are you sure to reset the addon |cffff0000[%s]|r configuration file?'] = '你確定要恢復插件|cffff0000[%s]|r的默認配置？'
+L['Are you sure to delete the selected item?'] = '你確定要刪除所選條目？'
+
+L['Addon Name:'] = '插件名稱：'
+L['Addon Version:'] = '插件版本：'
+L['Addon Author:'] = '插件作者：'
+L['Author Email:'] = '作者郵箱：'
+L['Author Weibo:'] = '作者微博：'
+L['Author GitHub:'] = '作者GitHub：'
+
+L['Select all'] = '全部選擇'
+L['Select none'] = '全部不選擇'
