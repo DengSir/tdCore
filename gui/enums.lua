0bad702f3349d04e8054764825152fada4b82644
DengSir <ldz5@qq.com>
2013-01-15 13:13:02 +0800

    some files upload

---
+++ gui/enums.lua
@@ -0,0 +1,18 @@
+
+local GUI = tdCore('GUI')
+
+GUI.DialogIcon = {
+    Information = [[Interface\HelpFrame\HelpIcon-Suggestion]],
+    Question    = [[Interface\HelpFrame\HelpIcon-KnowledgeBase]],
+    Warning     = [[Interface\HelpFrame\HelpIcon-ReportAbuse]],
+    Setting     = [[Interface\HelpFrame\HelpIcon-CharacterStuck]],
+    Critical    = [[Interface\HelpFrame\HelpIcon-Bug]],
+    Default     = [[Interface\HelpFrame\ReportLagIcon-Chat]],
+}
+
+GUI.ListButton = {
+    Add = 'Add',
+    Delete = 'Delete',
+    SelectAll = 'SelectAll',
+    SelectNone = 'SelectNone',
+}
